281df05967a5ae36b9ead5e13ddd119879894afe
Dan Davison <dandavison7@gmail.com>
2016-04-02 16:45:01 -0700

    Output hits and calling function name

--- call_graph/__init__.py
+++ call_graph/__init__.py
@@ -1,6 +1,14 @@
-from git_grep import grep
+from call_graph.cursor import Cursor
+from call_graph.cursor import Python
+from call_graph.cursor import get_enclosing_function
+from call_graph.git_grep import grep
 
 
 def get_call_sites(pattern):
-    hits = grep(pattern).strip().splitlines()
-    return hits
+    hits = grep(pattern)
+    for hit in hits:
+        with open(hit.path) as fp:
+            cursor = Cursor(fp, line=hit.line)
+        function = get_enclosing_function(cursor, Python)
+
+        print('%s:%d:%-90s %s' % (tuple(hit) + (function,)))
--- call_graph/git_grep.py
+++ call_graph/git_grep.py
@@ -1,5 +1,31 @@
+import re
 import subprocess
 
 
+class Hit:
+    regex = re.compile(r'([a-zA-Z0-9\./_-]+):(\d+):(.+)')
+
+    def __init__(self, path, line, text):
+        self.path = path
+        self.line = int(line)
+        self.text = text
+
+    @classmethod
+    def from_raw(cls, output):
+        match = re.match(Hit.regex, output.decode('utf-8'))
+        if not match:
+            raise ValueError(
+                "Cannot parse git grep output line: '%s'" % output)
+        else:
+            return cls(*match.groups())
+
+    def __iter__(self):
+        return iter((self.path, self.line, self.text))
+
+
 def grep(pattern):
-    return subprocess.check_output(['git', 'grep', '%s' % pattern])
+    output = subprocess.check_output([
+        'git', 'grep', '-n',
+        '%s' % pattern,
+    ])
+    return map(Hit.from_raw, output.strip().splitlines())
