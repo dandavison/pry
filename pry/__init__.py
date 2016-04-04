d8c02719f3a7c727f71c235b9c41f988b47a1264
Dan Davison <dandavison7@gmail.com>
2016-04-04 11:34:12 -0400

    print call graph

--- pry/__init__.py
+++ pry/__init__.py
@@ -7,11 +7,19 @@ from pry.languages import Python
 logger = logging.getLogger(__file__)
 debug = logger.debug
 
+MAX_CALL_SITES = 50
+TOO_MANY_CALL_SITES_MARKER = '<call_sites>'
 
 def make_call_graph(function):
     debug('make_call_graph(%s)' % function)
 
     call_sites = grep(function)
+
+    if len(call_sites) > MAX_CALL_SITES:
+        return {
+            TOO_MANY_CALL_SITES_MARKER: len(call_sites)
+        }
+
     call_graph = {}
     for call_site in call_sites:
         debug('call_site: %s' % call_site)
@@ -29,3 +37,13 @@ def make_call_graph(function):
             call_graph[parent_function] = make_call_graph(parent_function)
 
     return call_graph
+
+
+def print_call_graph(graph, level=0):
+    indent = '    ' * level
+    for key, subgraph in sorted(graph.items()):
+        if key == TOO_MANY_CALL_SITES_MARKER:
+            print('{indent}{key}: {subgraph}'.format(**locals()))
+        else:
+            print('{indent}{key}'.format(**locals()))
+            print_call_graph(subgraph, level + 1)
